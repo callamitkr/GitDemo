cd1b22e9b2c73c5da7c7fcdb2ec72a09bd534449
callamitkr <36655367+callamitkr@users.noreply.github.com>
2020-04-25 10:56:53 +0530

    Update DemoTest.java

--- src/test/java/TestPackageAmit/DemoTest.java
+++ src/test/java/TestPackageAmit/DemoTest.java
@@ -4,5 +4,7 @@ public class DemoTest {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+         System.out.println("Hello World");
+         System.out.println("Hello World rwerwere");
     }
 }
